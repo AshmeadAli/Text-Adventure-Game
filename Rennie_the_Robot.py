b4d8496caf70d7e849ef9cce54bcf01a3eaedbb6
AshmeadAli <31998065+AshmeadAli@users.noreply.github.com>
2017-09-15 11:59:59 -0700

    Add files via upload

---
+++ Rennie_the_Robot.py
@@ -0,0 +1,57 @@
+# RENNIE THE ROBOT
+# FUTURELEARN, SEPTEMBER 03,2017
+# Ashmead ALI
+
+from shapes import Triangle,Rectangle,Oval
+mouth = Rectangle()
+mouth.set_width(200)
+mouth.set_height(100)
+mouth.set_color('blue')
+mouth.set_x(200)
+mouth.set_y(450)
+mouth.draw()
+
+nose = Oval()
+nose.set_width(100)
+nose.set_height(100)
+nose.set_color('red')
+nose.set_x(250)
+nose.set_y(250)
+nose.draw()
+
+
+left_eye = Rectangle()
+left_eye.set_width(100)
+left_eye.set_height(100)
+left_eye.set_color('blue')
+left_eye.set_x(100)
+left_eye.set_y(100)
+left_eye.draw()
+
+left_pupil = Oval()
+left_pupil.set_width(100)
+left_pupil.set_height(100)
+left_pupil.set_color('red')
+left_pupil.set_x(150)
+left_pupil.set_y(100)
+left_pupil.draw()
+
+right_eye = Rectangle()
+right_eye.set_width(100)
+right_eye.set_height(100)
+right_eye.set_color('blue')
+right_eye.set_x(400)
+right_eye.set_y(100)
+right_eye.draw()
+
+right_pupil = Oval()
+right_pupil.set_width(100)
+right_pupil.set_height(100)
+right_pupil.set_color('red')
+right_pupil.set_x(350)
+right_pupil.set_y(100)
+right_pupil.draw()
+
+
+
+
