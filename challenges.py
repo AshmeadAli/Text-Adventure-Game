82609eb822513106529768b0ff20ebd8348a3f6b
AshmeadAli <31998065+AshmeadAli@users.noreply.github.com>
2017-09-16 10:37:23 -0700

    Add files via upload

---
+++ challenges.py
@@ -0,0 +1,10 @@
+# things to do
+
+# create the following classes:
+  # class Item()
+  # class Friend(Character)   # subclass
+
+# more interactions:
+  # input words : talk, fight
+  # end game if fight is lost
+  # add methods to put character to sleep, bribe, steal, hug, gift
